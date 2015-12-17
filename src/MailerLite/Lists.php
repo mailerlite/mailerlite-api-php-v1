c8bc570243052c5556a615267f4fe7a6bec8a096
Justinas Liubinskas <justinas.liubinskas@mailer.lt>
2015-12-17 13:53:14 +0200

    params added to lists class

--- src/MailerLite/Lists.php
+++ src/MailerLite/Lists.php
@@ -10,22 +10,22 @@ class Lists extends Base\Rest {
 		parent::__construct($apiKey);
 	}
 
-	public function getActive()
+	public function getActive($data = null)
 	{
 		$this->path .= 'active/';
-		return $this->execute('GET');
+		return $this->execute('GET', $data);
 	}
 
-	public function getUnsubscribed()
+	public function getUnsubscribed($data = null)
 	{
 		$this->path .= 'unsubscribed/';
-		return $this->execute('GET');
+		return $this->execute('GET', $data);
 	}
 
-	public function getBounced()
+	public function getBounced($data = null)
 	{
 		$this->path .= 'bounced/';
-		return $this->execute('GET');
+		return $this->execute('GET', $data);
 	}
 	
 }
